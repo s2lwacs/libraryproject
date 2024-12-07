1f711e1996bca423597381131e2c3543a97920fe
s2lwacs <salwaalharb7@gmail.com>
2024-12-07 15:37:52 +0000

    lab6 task2

--- libraryproject/apps/bookmodule/views.py
+++ libraryproject/apps/bookmodule/views.py
@@ -13,6 +13,20 @@ def viewbook(request, bookId):
     return render(request, 'bookmodule/one_book.html')
 
 def search(request):
+    if request.method == "POST":
+        string = request.POST.get('keyword').lower()
+        isTitle = request.POST.get('option1')
+        isAuthor = request.POST.get('option2')
+        # now filter
+        books = __getBooksList()
+        newBooks = []
+        for item in books:
+            contained = False
+            if isTitle and string in item['title'].lower(): contained = True
+            if not contained and isAuthor and string in item['author'].lower():contained = True
+            if contained: newBooks.append(item)
+        return render(request, 'bookmodule/bookList.html', {'books':newBooks})
+
     return render(request, 'bookmodule/search.html')
 
 def aboutus(request):
@@ -29,6 +43,11 @@ def viewbook(request, bookId):
     context = {'book':targetBook} 
     return render(request, 'bookmodule/show.html', context)
 
+def __getBooksList():
+    book1 = {'id':12344321, 'title':'Continuous Delivery', 'author':'J.Humble and D. Farley'}
+    book2 = {'id':56788765,'title':'Reversing: Secrets of Reverse Engineering', 'author':'E. Eilam'}
+    book3 = {'id':43211234, 'title':'The Hundred-Page Machine Learning Book', 'author':'Andriy Burkov'}
+    return [book1, book2, book3]
 
 def links(request):
     return render(request, 'bookmodule/links.html')
